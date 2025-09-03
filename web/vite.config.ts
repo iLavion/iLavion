9798118b7a65620d99efda89fb31ba9a1fda77bf
Lavion <ilavion4u@gmail.com>
2025-09-03 14:44:38 +0200

    init vite solidjs using bun for a portfolio website

---
+++ web/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    solid()
+  ],
+  resolve: {
+    alias: {
+      '~': '/src',
+    },
+  },
+  build: {
+    target: 'esnext',
+  },
+});
